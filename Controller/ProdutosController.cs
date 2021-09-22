e55eb72d4e0f2eea619f05ca94f97dd06e500cdb
José VItor <jose81073175@gmail.com>
2021-09-22 05:03:50 -0300

    Add method post

--- Controller/ProdutosController.cs
+++ Controller/ProdutosController.cs
@@ -4,6 +4,7 @@ using System.Collections.Generic;
 using Microsoft.AspNetCore.Mvc;
 using APICatalogo.Context;
 using APICatalogo.Entitys;
+using Microsoft.EntityFrameworkCore;
 
 namespace APICatalogo.Controller
 {
@@ -23,12 +24,31 @@ namespace APICatalogo.Controller
         [HttpGet]
         public ActionResult<IEnumerable<Produto>> Get()
         {
-            return _context.Produtos.ToList();    
+            return _context.Produtos.AsNoTracking().ToList();    
+        }
+
+        [HttpGet("{id}",Name ="ObterProduto")]
+        public ActionResult<Produto> Get(int id)
+        {
+            var produto = _context.Produtos.AsNoTracking().FirstOrDefault(prop => prop.ProdutoId == id);
+
+            if(produto == null)
+            {
+                return NotFound();
+            }
+            return produto; 
+         }
+
+        [HttpPost]
+        public ActionResult Post([FromBody]Produto produto)
+        {
+            _context.Produtos.Add(produto);
+            _context.SaveChanges();
+
+            return new CreatedAtRouteResult("ObterProduto", 
+            new { id = produto.ProdutoId }, produto);
         }
     }
-
-
-
 }
 
 
--- Entitys/Produto.cs
+++ Entitys/Produto.cs
@@ -5,7 +5,7 @@ using System.ComponentModel.DataAnnotations.Schema;
 namespace APICatalogo.Entitys
 {
 
-	[Table("Produtos")]
+    [Table("Produtos")]
 	public class Produto
 	{
 
